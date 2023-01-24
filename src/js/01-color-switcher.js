091b4b7ca937c4cd6980da309e0e7b33d64c49cc
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-01-24 19:19:02 +0200

    hw-09-settings-demo

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+// const refs = {
+//     body: document.querySelector('body'),
+//     btnStart: document.querySelector('button[data-start]'),
+//     btnStop: document.querySelector('button[data-stop]'),
+// };
+
+// const bgColorBody = getRandomHexColor();
+
+// refs.btnStart.addEventListener('click', onStartBtnClick);
+// // refs.btnStop.addEventListener('click', onStopBtnClick);
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+
+// function  onStartBtnClick() {
+//   setInterval(() => {
+//     console.log(bgColorBody);
+//   }, 1000);
+// };
